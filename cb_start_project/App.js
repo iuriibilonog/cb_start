9020a326dce741728d8eeef5ef5e1ccc23745e6b
Iurii Bilonog <iuriibilonog@gmail.com>
2023-09-04 13:21:13 +0300

    add interceptor

--- cb_start_project/App.js
+++ cb_start_project/App.js
@@ -1,11 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import api from "./src/services/interceptor";
 
 export default function App() {
+  const handlePress = async () => {
+    try {
+      const data = await api.get();
+      console.log("data", data.status);
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <TouchableOpacity onPress={handlePress}>
+        <Text>Press</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -13,8 +25,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ cb_start_project/src/services/interceptor.js
@@ -0,0 +1,88 @@
+import axios from "axios";
+import {
+  getDataFromStorage,
+  setDataToStorage,
+  clearAsyncStorage,
+} from "../helpers/asyncStorageHelpers";
+// import { useContext } from 'react';
+// import { UIContext } from 'contexts/UIContext';
+
+const url = "https://httpstat.us/404";
+
+const api = axios.create({
+  withCredentials: true,
+  baseURL: `${url}`,
+});
+
+export const AxiosInterceptor = ({ children }) => {
+  api.interceptors.request.use((config) => {
+    config.headers = {
+      "Content-Language": getDataFromStorage("lang") || "en",
+      "X-Requested-With": "XMLHttpRequest",
+      "X-Access-Token": getDataFromStorage("token") || null,
+      // 'X-Client-Version': window.COMMIT_SHA || null,
+      "Content-Type": "application/json",
+    };
+
+    return config;
+  });
+
+  api.interceptors.response.use(
+    (config) => {
+      return config;
+    },
+    async (error) => {
+      const originalRequest = error.config;
+      const refreshToken = getDataFromStorage("refresh");
+
+      if (error?.response?.status === 401 && !refreshToken) {
+        return (window.location = "/session-expired");
+      }
+
+      //   if (
+      //     error?.response?.status === 401 &&
+      //     error?.config &&
+      //     !error?.config?.secondTime &&
+      //     refreshToken
+      //   ) {
+      //     originalRequest.secondTime = true;
+      //     try {
+      //       const headers = {
+      //         "X-Access-Token": getDataFromStorage("token"),
+      //       };
+      //       const response = await axios.post(
+      //         `${url}/auth/refresh`,
+      //         {
+      //           withCredentials: true,
+      //           refresh_token: refreshToken,
+      //         },
+      //         {
+      //           headers: headers,
+      //         }
+      //       );
+      //       setDataToStorage("token", response.data.access_token);
+      //       setDataToStorage("refresh", response.data.refresh_token);
+
+      //       return api.request(originalRequest);
+      //     } catch (err) {
+      //       if (err?.response?.status === 401) {
+      //         window.location = "/session-expired";
+
+      //         clearAsyncStorage();
+      //         throw err;
+      //       }
+      //       //   else {
+      //       //     context.showErrorInterceptor(err);
+      //       //   }
+      //     }
+      //   }
+      //   else {
+      //     context.showErrorInterceptor(error);
+      //   }
+    }
+  );
+
+  return children;
+};
+
+export default api;
