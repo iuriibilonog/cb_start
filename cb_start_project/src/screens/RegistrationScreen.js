d8750ad7b3198cfbbe1f27b6d316f105cc65208f
Lukavenko Sergii <myjavaname@gmail.com>
2023-09-04 15:44:41 +0300

    login screen - layout - done

--- cb_start_project/src/screens/RegistrationScreen.js
+++ cb_start_project/src/screens/RegistrationScreen.js
@@ -10,19 +10,25 @@ import {
   ImageBackground,
   Image,
   TextInput,
+  Button,
+  TouchableOpacity,
 } from 'react-native';
 // import StyledTextInputBox from 'src/components/molecules/StyledTextInputBox';
 // import StyledButton from 'src/components/atoms/StyledButton/StyledButton';
-// const addPhotoIcon = require('src/assets/images/plus_in_round.png');
+
+const boxEmpty = require('src/images/box_empty.png');
+const boxChecked = require('src/images/box_checked.png');
+const eyeOff = require('src/images/eye_off.png');
+const eyeOn = require('src/images/eye_on.png');
 
 const RegistrationScreen = ({ navigation }) => {
-  const handleNextScreen = () => {
-    console.log('nextScreen>>');
-    navigation.navigate('RegistrationScreen');
+  const handleSubmit = (data) => {
+    console.log('submit data>>', inputValue);
+    // navigation.navigate('RegistrationScreen');
   };
 
   const [inputValue, setInputValue] = useState('');
-  //   const [isInputFocused, setIsInputFocused] = useState(false);
+  const [isRememberMe, setIsRememberMe] = useState(false);
   const [isPassShown, setIsPassShown] = useState(false);
 
   const handlePassShowBtn = () => {
@@ -48,7 +54,7 @@ const RegistrationScreen = ({ navigation }) => {
         onChangeText={(text) => handleInput({ email: text })}
       />
       <TextInput
-        style={styles.input}
+        style={{ ...styles.input, marginVertical: 30 }}
         placeholder="login"
         placeholderTextColor={'grey'}
         value={inputValue?.name}
@@ -63,10 +69,19 @@ const RegistrationScreen = ({ navigation }) => {
           onChangeText={(text) => handleInput({ password: text })}
           secureTextEntry={!isPassShown}
         />
-        <Text style={styles.showPass} onPress={handlePassShowBtn}>
-          {isPassShown ? 'hide' : 'show'}
-        </Text>
+        <Pressable onPress={handlePassShowBtn} style={{ position: 'absolute', right: 20, top: 13 }}>
+          <Image source={isPassShown ? eyeOff : eyeOn} style={{ width: 25, height: 25 }} />
+        </Pressable>
       </View>
+      {/* <View style={styles.rememberWrapper}> */}
+      <Pressable onPress={() => setIsRememberMe((prev) => !prev)} style={styles.rememberWrapper}>
+        <Image source={isRememberMe ? boxChecked : boxEmpty} style={{ width: 25, height: 25 }} />
+        <Text style={styles.rememberTxt}>Remember me</Text>
+      </Pressable>
+      {/* </View> */}
+      <TouchableOpacity style={styles.submitBtn} onPress={handleSubmit}>
+        <Text> Sign Up </Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -82,7 +97,6 @@ const styles = StyleSheet.create({
   input: {
     width: '100%',
     height: 50,
-    marginBottom: 30,
     color: '#36D0BB',
     borderWidth: 1,
     borderColor: 'rgba(54, 208, 187, 0.20)',
@@ -90,7 +104,27 @@ const styles = StyleSheet.create({
     textAlign: 'center',
   },
   passInputWrapper: { width: '100%', position: 'relative' },
-  showPass: { position: 'absolute', right: 16, top: 15, color: 'rgba(54, 208, 187, 0.20)' },
+  rememberWrapper: {
+    width: '100%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 12,
+    marginBottom: 30,
+    opacity: 0.5,
+  },
+  rememberTxt: {
+    marginLeft: 12,
+    color: '#fff',
+  },
+  submitBtn: {
+    width: '100%',
+    height: 50,
+    borderRadius: 2,
+    backgroundColor: '#36D0BB',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  submitTxt: { fontSize: 20, fontWeight: '600', letterSpacing: 1 },
 });
 
 export default RegistrationScreen;
