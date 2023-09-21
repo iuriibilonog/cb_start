19f5726a3d880863cb28ecfd7af7bf2fe3825b6e
Lukavenko Sergii <myjavaname@gmail.com>
2023-09-21 18:53:38 +0300

    pagination- done )

--- cb_start_project/src/components/molecules/Pagination.js
+++ cb_start_project/src/components/molecules/Pagination.js
@@ -24,10 +24,26 @@ const Pagination = ({ totalPages = 20, currentPage = 1, setCurrentPage }) => {
   const [pagesLine, setPagesLine] = useState([2, 3, 4, 5]);
   const [isFirstDotsShow, setIsFirstDotsShow] = useState(false);
   const [isLastDotsShow, setIsLastDotsShow] = useState(false);
-  const [inputValue, setInputValue] = useState('');
-  const [isShowInput, setIsShowInput] = useState(false);
+  const [inputValue, setInputValue] = useState();
+  const [isLeftShowInput, setIsLeftShowInput] = useState(false);
+  const [isRightShowInput, setIsRightShowInput] = useState(false);
+  const [isKeyboardVisible, setIsKeyboardVisible] = useState(false);
 
   const headerHeight = useHeaderHeight();
+
+  useEffect(() => {
+    const keyboardDidShowListener = Keyboard.addListener('keyboardDidShow', () => {
+      setIsKeyboardVisible(true);
+    });
+    const keyboardDidHideListener = Keyboard.addListener('keyboardDidHide', () => {
+      setIsKeyboardVisible(false);
+    });
+
+    return () => {
+      keyboardDidHideListener.remove();
+      keyboardDidShowListener.remove();
+    };
+  }, []);
 
   useEffect(() => {
     let res = [];
@@ -38,6 +54,16 @@ const Pagination = ({ totalPages = 20, currentPage = 1, setCurrentPage }) => {
 
     setPagesLine(res);
   }, [totalPages]);
+
+  useEffect(() => {
+    if (!isKeyboardVisible && inputValue) {
+      const numberValue = parseInt(inputValue);
+      setSelected(numberValue > totalPages ? totalPages : numberValue < 1 ? 1 : numberValue);
+      setIsLeftShowInput(false);
+      setIsRightShowInput(false);
+      setInputValue('');
+    }
+  }, [isKeyboardVisible]);
 
   useEffect(() => {
     console.log('selected', selected);
@@ -66,92 +92,97 @@ const Pagination = ({ totalPages = 20, currentPage = 1, setCurrentPage }) => {
     setSelected((prev) => prev - 1);
   };
 
-  const handleOnBlur = () => {
-    console.log('inputValue', inputValue);
-    setSelected(parseInt(inputValue));
-    setIsShowInput(false);
-    // Keyboard.dismiss();
-  };
-
   return (
-    <TouchableWithoutFeedback onPress={handleOnBlur}>
-      <KeyboardAvoidingView
-        keyboardVerticalOffset={headerHeight}
-        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-        // behavior="position"
-        // style={{ flex: 1 }}
-      >
-        <View style={styles.wrapper}>
-          <TouchableOpacity activeOpacity={0.5} onPress={arrowLeftBtn}>
-            <Image source={arrowLeft} style={styles.arrow} />
-          </TouchableOpacity>
-          <TouchableOpacity activeOpacity={0.5} onPress={() => setSelected(1)}>
-            <View style={styles.itemWrapper}>
-              <SimpleText
-                style={{
-                  ...styles.itemText,
-                  fontFamily: selected === 1 ? 'Mont_SB' : 'Mont',
-                  color: selected === 1 ? '#212121' : '#96979B',
-                }}
-              >
-                1
-              </SimpleText>
-            </View>
-          </TouchableOpacity>
+    <KeyboardAvoidingView
+      keyboardVerticalOffset={headerHeight}
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      // behavior="position"
+      // style={{ flex: 1 }}
+    >
+      <View style={styles.wrapper}>
+        <TouchableOpacity activeOpacity={0.5} onPress={arrowLeftBtn}>
+          <Image source={arrowLeft} style={styles.arrow} />
+        </TouchableOpacity>
+        <TouchableOpacity activeOpacity={0.5} onPress={() => setSelected(1)}>
+          <View style={styles.itemWrapper}>
+            <SimpleText
+              style={{
+                ...styles.itemText,
+                fontFamily: selected === 1 ? 'Mont_SB' : 'Mont',
+                color: selected === 1 ? '#212121' : '#96979B',
+              }}
+            >
+              1
+            </SimpleText>
+          </View>
+        </TouchableOpacity>
 
-          {isFirstDotsShow &&
-            (isShowInput ? (
-              <TextInput
-                style={styles.dots}
-                autoFocus={true}
-                value={inputValue}
-                onChangeText={setInputValue}
-              />
-            ) : (
-              <TouchableOpacity onPress={() => setIsShowInput(true)}>
-                <SimpleText style={styles.dots}>...</SimpleText>
-              </TouchableOpacity>
-            ))}
-          {pagesLine &&
-            pagesLine.map((item, index) => (
-              <TouchableOpacity key={index} activeOpacity={0.5} onPress={() => setSelected(item)}>
-                <View style={styles.itemWrapper}>
-                  <SimpleText
-                    style={{
-                      ...styles.itemText,
-                      fontFamily: item === selected ? 'Mont_SB' : 'Mont',
-                      color: item === selected ? '#212121' : '#96979B',
-                    }}
-                  >
-                    {item}
-                  </SimpleText>
-                </View>
-              </TouchableOpacity>
-            ))}
-          {isLastDotsShow && <SimpleText style={styles.dots}>...</SimpleText>}
-          {totalPages > 1 && (
-            <TouchableOpacity activeOpacity={0.5} onPress={() => setSelected(totalPages)}>
+        {isFirstDotsShow &&
+          (isLeftShowInput ? (
+            <TextInput
+              style={styles.dots}
+              keyboardType="numeric"
+              autoFocus={true}
+              value={inputValue}
+              onChangeText={setInputValue}
+            />
+          ) : (
+            <TouchableOpacity onPress={() => setIsLeftShowInput(true)}>
+              <SimpleText style={styles.dots}>...</SimpleText>
+            </TouchableOpacity>
+          ))}
+        {pagesLine &&
+          pagesLine.map((item, index) => (
+            <TouchableOpacity key={index} activeOpacity={0.5} onPress={() => setSelected(item)}>
               <View style={styles.itemWrapper}>
                 <SimpleText
                   style={{
                     ...styles.itemText,
-                    fontFamily: selected === totalPages ? 'Mont_SB' : 'Mont',
-                    color: selected === totalPages ? '#212121' : '#96979B',
+                    fontFamily: item === selected ? 'Mont_SB' : 'Mont',
+                    color: item === selected ? '#212121' : '#96979B',
                   }}
                 >
-                  {totalPages}
+                  {item}
                 </SimpleText>
               </View>
             </TouchableOpacity>
-          )}
-          {/* <TouchableOpacity activeOpacity={0.5} onPress={() => setSelected((prev) => prev + 1)}> */}
-
-          <TouchableOpacity activeOpacity={0.5} onPress={arrowRightBtn}>
-            <Image source={arrowRight} style={styles.arrow} />
+          ))}
+        {isLastDotsShow &&
+          (isRightShowInput ? (
+            <TextInput
+              style={styles.dots}
+              keyboardType="numeric"
+              autoFocus={true}
+              value={inputValue}
+              onChangeText={setInputValue}
+            />
+          ) : (
+            <TouchableOpacity onPress={() => setIsRightShowInput(true)}>
+              <SimpleText style={styles.dots}>...</SimpleText>
+            </TouchableOpacity>
+          ))}
+        {totalPages > 1 && (
+          <TouchableOpacity activeOpacity={0.5} onPress={() => setSelected(totalPages)}>
+            <View style={styles.itemWrapper}>
+              <SimpleText
+                style={{
+                  ...styles.itemText,
+                  fontFamily: selected === totalPages ? 'Mont_SB' : 'Mont',
+                  color: selected === totalPages ? '#212121' : '#96979B',
+                }}
+              >
+                {totalPages}
+              </SimpleText>
+            </View>
           </TouchableOpacity>
-        </View>
-      </KeyboardAvoidingView>
-    </TouchableWithoutFeedback>
+        )}
+        {/* <TouchableOpacity activeOpacity={0.5} onPress={() => setSelected((prev) => prev + 1)}> */}
+
+        <TouchableOpacity activeOpacity={0.5} onPress={arrowRightBtn}>
+          <Image source={arrowRight} style={styles.arrow} />
+        </TouchableOpacity>
+      </View>
+    </KeyboardAvoidingView>
   );
 };
 
